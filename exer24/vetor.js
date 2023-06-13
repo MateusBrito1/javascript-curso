c6cbfdbb63bf56d44f24b8ee88e28745f717c191
Mateus.Brito <mateusbritocontact@gmail.com>
2023-06-12 22:40:09 -0300

    att

--- exer24/vetor.js
+++ exer24/vetor.js
@@ -4,6 +4,6 @@ for (let pos=0; pos < valores.length; pos++){
     console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)      //Modo mais extenso.
 } */
 
-for (let pos in valores){                               //Modo mais simplificado e melhor.
+for (let pos in valores){                               //Modo mais simplificado e melhor. (Lendo = Para cada posição em valores.)
     console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)     
 }
