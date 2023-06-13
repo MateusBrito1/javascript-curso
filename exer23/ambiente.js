1ca3195452a58e971b13f38d605e1974eb35973a
Mateus.Brito <mateusbritocontact@gmail.com>
2023-06-12 22:36:38 -0300

    att

--- exer23/ambiente.js
+++ exer23/ambiente.js
@@ -1,13 +1,13 @@
 let num = [5, 8, 9, 3, 5]
-num.push(1)
-num.sort()
+num.push(1)         //Atribue um valor a ultima casa das variaveis.
+num.sort()          //Deixa o conteudo (valor da variavel) em ordem crescente.
 console.log(num)
 console.log (`O vetor tem ${num.length} posições`)
 console.log (`O primeiro valor do vetor é ${num[0]}`)
 
 
-let pos = num.indexOf(9)
-    if (pos == -1){
+let pos = num.indexOf(9)              //Procura se tem o valor 9 nas variaveis.
+    if (pos == -1){                    //Se não foi encontrado, o valor fica -1, logo, se a 'pos' for igual a -1, irá mostrar que não encontrou  o valor.
         console.log(`o Valor não foi encontrado`)
     }   else {
         console.log(`o valor está na posição ${pos}`)
--- exer24/vetor.js
+++ exer24/vetor.js
@@ -1,9 +1,9 @@
 let valores = [8, 8, 7, 5, 1, 6, 0, 7]
 /*
 for (let pos=0; pos < valores.length; pos++){
-    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)      //Modo mais extenso.
 } */
 
-for (let pos in valores){
-    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)
+for (let pos in valores){                               //Modo mais simplificado e melhor.
+    console.log(`A posição ${pos} tem o valor de ${valores[pos]}`)     
 }
