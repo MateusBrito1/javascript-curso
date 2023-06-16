0f369048597b5c977fc63cbf6a758351297b3e96
Mateus.Brito <mateusbritocontact@gmail.com>
2023-06-16 18:58:08 -0300

    att

--- Udemy/Aula 8 copy/assets/js/main.js
+++ Udemy/Aula 8 copy/assets/js/main.js
@@ -1,14 +1,16 @@
 //Caputar o evento de submit do formulario
-let peso = document.getElementById('peso')
-let altura = document.getElementById('altura')
 const form = document.getElementById('form')
-
-
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     console.log('Evento previnido.')
+    pegarResultado('Olá mundo!')
 });
+
+function pegarResultado (msg){
+    const resultado = document.getElementById('resultado')
+    resultado.innerHTML = `<p>${msg}</p>`
+}
 
 
 
