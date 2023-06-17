bd38e21d5699803362b62676b8858efc0535a5c6
Mateus.Brito <mateusbritocontact@gmail.com>
2023-06-16 22:12:21 -0300

    att

--- Udemy/Aula 8 copy/assets/js/main.js
+++ Udemy/Aula 8 copy/assets/js/main.js
@@ -1,17 +1,2 @@
-//Caputar o evento de submit do formulario
-const form = document.getElementById('form')
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log('Evento previnido.')
-    pegarResultado('Olá mundo!')
-});
-
-function pegarResultado (msg){
-    const resultado = document.getElementById('resultado')
-    resultado.innerHTML = `<p>${msg}</p>`
-}
-
-
 
 
