eb9a950da65c7ea483e7f2adf151baa8e6490ec3
Mateus.Brito <mateusbritocontact@gmail.com>
2023-06-20 18:43:43 -0300

    att

---
+++ Udemy/Aula 21/breakcontinue.js
@@ -0,0 +1,13 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i in numeros){
+    let numero = numeros[i]
+
+    console.log(numeros)
+
+    if (numeros === 7){
+        console.log('7 encontrado')
+        break;
+    }
+
+}
